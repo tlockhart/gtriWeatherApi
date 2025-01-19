bc00d508f70b2af30bc542857a93547a6eccaa78
Tony Lockhart <tony.lockhart@ymail.com>
2025-01-19 04:38:22 -0500

    chore: fixed syntax error

--- client/src/utils/getCurrentForecast.js
+++ client/src/utils/getCurrentForecast.js
@@ -1,6 +1,6 @@
 const getCurrentForecast = async (stationId) => {
-    console.log("Route:", REACT_APP_API_URL)
-    return fetch(`{REACT_APP_API_URL}/api/weather/${stationId}`, {
+    console.log("Route:", process.env.REACT_APP_API_URL)
+    return fetch(`{process.env.REACT_APP_API_URL}/api/weather/${stationId}`, {
         method: "GET", 
         headers: {
             "Content-Type": "application/json"
